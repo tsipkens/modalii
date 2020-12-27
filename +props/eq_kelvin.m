10e79bafa6d485ac58170e21d84d2a894d14f730
tsipkens <tsipkens@gmail.com>
2020-12-27 11:45:35 -0500

    Update props.eq_kelvin
    Appropriately references props.eq_claus_clap.

--- +props/eq_kelvin.m
+++ +props/eq_kelvin.m
@@ -1,7 +1,7 @@
 
 function [pv] = eq_kelvin(prop, T, dp, hv)
 
-pv0 = props.eq_claus_clap(T, dp, hv); % Clausius-Clapeyron equation
+pv0 = props.eq_claus_clap(prop, T, dp, hv); % Clausius-Clapeyron equation
 pv = pv0 .* exp((4 * prop.gamma(dp, T)) ./ ...
     ((dp) .* prop.rho(T) .* prop.Rs .* T)); % Evaluate the Kelvin Eqn.
 
