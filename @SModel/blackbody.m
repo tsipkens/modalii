1db75dc2492fc60b3bada3637eeda913cb06c8ec
tsipkens <tsipkens@gmail.com>
2020-12-28 01:58:29 -0500

    Commenting update to SModel.blackbody

--- @SModel/blackbody.m
+++ @SModel/blackbody.m
@@ -1,23 +1,23 @@
 
 % BLACKBODY  Vector implementation of Planck's law.
 % AUTHOR: Timothy Sipkens
-%=========================================================================%
-
-function Ib = blackbody(T,l)
-%-------------------------------------------------------------------------%
-% Inputs:
+% 
+% INPUTS: 
 %   l	Wavelength, [nm]
 %   T	Temperature, [K]
 %
-% Outputs:
+% OUTPUTS:
 %   Ib  Blackbody intensity
-%-------------------------------------------------------------------------%
+%=========================================================================%
 
-p0 = bsxfun(@times,T,reshape(l.*1e-9,1,1,[]));
-p1 = 1./(exp(0.0143877696./(p0))); % Number is phi=h*c/kB
-    % Wein's approximation used in forward model for scaling factor paper
-p2 = 1.191042868e-16./(l.*1e-9).^5; % Number is 2*h*c^2
+function Ib = blackbody(T,l)
 
-Ib = bsxfun(@times,p1,reshape(p2,1,1,[]));
+p0 = bsxfun(@times, T, reshape(l .* 1e-9, 1, 1, []));
+
+% Wein's approximation used in forward model for scaling factor paper
+p1 = 1 ./ (exp(0.0143877696 ./ (p0))); % Number is phi=h*c/kB
+p2 = 1.191042868e-16 ./ (l .* 1e-9) .^ 5; % constant givne by 2*h*c^2
+
+Ib = bsxfun(@times, p1, reshape(p2, 1, 1, []));
 
 end
