9df3124c3cbdf602ec4f1734a6d7ee3fca3550e5
tsipkens <tsipkens@gmail.com>
2020-12-28 02:19:06 -0500

    Remove deprecated Prop class

--- @Prop/eq_antoine.m
+++
@@ -1,6 +0,0 @@
-
-function [pv] = eq_antoine(prop,T,dp,hv)
-
-pv = exp(prop.C-prop.C1./(T+prop.C2));
-
-end
--- @Prop/eq_tolman.m
+++
@@ -1,7 +0,0 @@
-
-function [gamma] = eq_tolman(prop, T, dp)
-
-gamma = prop.gammaT(T)./(1+4*(prop.delta.*1e-9)./dp);
-
-end
-
