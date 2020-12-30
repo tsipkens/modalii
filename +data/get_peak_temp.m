334c740a2b3cc27ece54c93c4d5658279dd18b7c
tsipkens <tsipkens@gmail.com>
2020-12-30 12:33:39 -0500

    Bug fix for get_peak_temp

--- +data/get_peak_temp.m
+++ +data/get_peak_temp.m
@@ -1,5 +1,5 @@
 
-function [Tp,tp] = get_peak_temp(s,varargin)
+function [Tp,tp] = get_peak_temp(s, varargin)
 
 [s,t,l] = data.parse_signal(s);
 
@@ -8,17 +8,18 @@ ind_med = median(ind_max); % use median over shots
     % (Note: does not incorporate temporal laser jitter)
 tp = t(ind_med); % peak time
 
-if isa(varargin{1},'Prop')
+if isa(varargin{1}, 'SModel')
+    smodel = varargin{1};
+    prop = smodel.prop;
+elseif isa(varargin{1}, 'struct')
     prop = varargin{1};
     prop.l = l; % ensure correct wavelengths are used
-    smodel = SModel(prop,{},{},signal);
-elseif isa(varargin{1},'SModel')
-    smodel = varargin{1};
+    smodel = SModel(prop, {}, {}, signal);
 else
     disp('Invalid input.');
 end
 
-[~,Tp] = smodel.IModel(nanmean(s(ind_med,:,:))); % calculate temperature
+[~,Tp] = smodel.IModel(prop, nanmean(s(ind_med,:,:))); % calculate temperature
 
 end
 
