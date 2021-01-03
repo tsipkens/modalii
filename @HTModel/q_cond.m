8174268665bddc2423b4f61c841d37d28b55cc59
tsipkens <tsipkens@gmail.com>
2021-01-03 14:35:05 -0500

    Update q_cond to include continuum function
    This is in addition to the existing free molecular function. Currently, the continuum regime expression remains inacccessible.

--- @HTModel/q_cond.m
+++ @HTModel/q_cond.m
@@ -1,6 +1,7 @@
 
 % Q_COND Rate of free-molecular conduction energy loss from the nanoparticle.
 % AUTHOR: Timothy Sipkens, 2018-12-17
+% Contains code snippets from K. J. Daun.
 % 
 % INPUTS:
 %   T       Vector of nanoparticle temperature, [K]
@@ -13,11 +14,33 @@
 function [q] = q_cond(htmodel, prop, T, dp)
 
 dp = dp .* 1e-9; % convert to meters so everything is in SI units
-prop = htmodel.prop;
+q = q_fm(prop, T, dp);  % compute free-molecular conduction
+
+end
+
+
+
+%== Q_FM =================================================================%
+%   Free molecular conduction, using the thermal accommodation coefficient
+%   and gamma2 (related to degrees of freedom) as a function of temperature.
+function [q] = q_fm(prop, T, dp)
+
 prop.alpha = min(max(prop.alpha, 0), 1); % added to force constraints
 q = ((prop.alpha * prop.Pg * prop.ct(prop) * pi .* ...
     (dp .^ 2) ./ (8 * prop.Tg)) .* ...
     prop.gamma2(T) .* (T - prop.Tg));
 
 end
+
+
+%== Q_CONT ===============================================================%
+%   Continuum regime condutions, using Eq. (6) in Daun and Hubermann. 
+%   Requires prop.k to be specified for the given material.
+%   NOTE: Currently not used.
+function [q] = q_cont(prop, T, dp)
+
+q = 2 .* pi .* dp .* integral(@(T) prop.k(T), prop.Tg, T);
+
+end
+
 
