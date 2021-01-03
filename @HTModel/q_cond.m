b1e48276fbd5955bfce68f1bd57d0e1974567409
tsipkens <tsipkens@gmail.com>
2021-01-03 14:53:33 -0500

    Initial (buggy) implementation of transition conduction
    + Added mean free path function

--- @HTModel/q_cond.m
+++ @HTModel/q_cond.m
@@ -14,7 +14,22 @@
 function [q] = q_cond(htmodel, prop, T, dp)
 
 dp = dp .* 1e-9; % convert to meters so everything is in SI units
-q = q_fm(prop, T, dp);  % compute free-molecular conduction
+
+switch htmodel.cond
+    case 'free-molecular'
+        q = q_fm(prop, T, dp, prop.Tg);  % compute free-molecular conduction
+        
+    case 'continuum'
+        q = q_cont(prop, T, dp, prop.Tg);  % compute continuum regime conduction
+        
+    case {'transition', 'fuchs'}
+        res = @(Tdelta) q_fm(prop, T, dp, Tdelta) - ...
+            qcont(prop, Tdelta, dp + 2*get_mfp(prop), prop.Tg);
+        
+        Tdelta = fzero(@(T) res(T), [prop.Tg, T]);
+        q = q_fm(prop, T, dp, Tdelta);
+
+end
 
 end
 
@@ -23,24 +38,40 @@ end
 %== Q_FM =================================================================%
 %   Free molecular conduction, using the thermal accommodation coefficient
 %   and gamma2 (related to degrees of freedom) as a function of temperature.
-function [q] = q_fm(prop, T, dp)
+function [q] = q_fm(prop, T, dp, Tg)
 
 prop.alpha = min(max(prop.alpha, 0), 1); % added to force constraints
 q = ((prop.alpha * prop.Pg * prop.ct(prop) * pi .* ...
-    (dp .^ 2) ./ (8 * prop.Tg)) .* ...
-    prop.gamma2(T) .* (T - prop.Tg));
+    (dp .^ 2) ./ (8 * Tg)) .* ...
+    prop.gamma2(T) .* (T - Tg));
 
 end
+
 
 
 %== Q_CONT ===============================================================%
 %   Continuum regime condutions, using Eq. (6) in Daun and Hubermann. 
-%   Requires prop.k to be specified for the given material.
-%   NOTE: Currently not used.
-function [q] = q_cont(prop, T, dp)
+%   REQUIRES prop.k (conductivity) to be specified for the gas.
+function [q] = q_cont(prop, T, dp, Tg)
 
-q = 2 .* pi .* dp .* integral(@(T) prop.k(T), prop.Tg, T);
+q = 2 .* pi .* dp .* integral(@(T) prop.k(T), Tg, T);
 
 end
+
+
+
+%== GET_MFP ==============================================================%
+%   Returns the Maxwell mean free path of the gas in nm based 
+%   on viscosity (Eq. 16 in %Huberman and Daun) Pg is pressure in Pa, 
+%   Tg is temperature in K, mg is molecular mass in kg.
+%   REQUIRES prop.mu (dynamic viscosity) to be specified for the gas.
+function lambda = get_mfp(prop)
+
+rho = prop.mg * prop.Pg / (prop.kB * prop.Tg);
+
+lambda = prop.mu / rho / sqrt(2*prop.kB .* prop.Tg / (pi * prop.mg));
+
+end
+
 
 
