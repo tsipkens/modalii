c67564cceb9e095f6403b0a27f31b66d55cc56e6
tsipkens <tsipkens@gmail.com>
2021-04-22 00:01:09 -0400

    Update load_config.m
    Allows for if/else/then statements.
    Currently still unused.

--- +tools/load_config.m
+++ +tools/load_config.m
@@ -16,32 +16,68 @@ for ii=1:length(fnames)
     
     prop0 = read_json(fnames{ii});  % read new settings
     
-    % Copy (or overwrite) existing settings.
+    
     f = fieldnames(prop0);
+    
+    % Copy (or overwrite) existing settings.
     for jj = 1:length(f)
         
         % Attempt to interpret Matlab expressions.
-        if isa(prop0.(f{jj}), 'char')
-            [converted, success] = str2num(prop0.(f{jj}));
-            if success
-                prop0.(f{jj}) = converted;
-                continue;
-            end
-            
-            try  % try 'eval(...)'
-                e0 = eval(prop0.(f{jj}));
-            catch
-                continue;  % just continue
-            end
-            if exist('e0', 'var')
-                prop0.(f{jj}) = e0;
-            end
-        end
+        prop0.(f{jj}) = interpret(prop0.(f{jj}));
         
         % Overwrite existing configuration.
         prop.(f{jj}) = prop0.(f{jj});
     end
     
+    %-- Handle if/then/else statements ------%
+    idx = find(contains(f, "if"));
+    
+    % Look for if... then... else pattern.
+    idx(idx == length(f)) = [];  % remove from flag if last item
+    idx(~contains(f(idx + 1), "then")) = [];
+    idx(~contains(f(idx + 2), "else")) = [];
+    
+    for ii=idx
+        if prop0.(f{ii})
+            fj = fieldnames(prop0.(f{ii+1}));
+            for jj=1:length(fj)
+                t0 = interpret(prop0.(f{ii+1}).(fj{jj}));
+                prop.(fj{jj}) = t0;
+            end
+        else
+            fj = fieldnames(prop0.(f{ii+2}));
+            for jj=1:length(fj)
+                t0 = interpret(prop0.(f{ii+2}).(fj{jj}));
+                prop.(fj{jj}) = t0;
+            end
+        end
+        
+        prop = rmfield(prop, f{ii});
+        prop = rmfield(prop, f{ii+1});
+        prop = rmfield(prop, f{ii+2});
+    end
+    
+end
+
+end
+
+
+
+% INTERPRET  Attempt to interpret Matlab expressions.
+function e0 = interpret(f)
+
+if isa(f, 'char')
+    [e0, success] = str2num(f);
+    if success; return; end
+
+    try  % try 'eval(...)'
+        e0 = eval(f);
+    catch
+        e0 = f;
+        return;  % just continue
+    end
+else
+    e0 = f;
 end
 
 end
