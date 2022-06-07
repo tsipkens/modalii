ffb9e7ddfc2aa78bea2ef67f6336f08439834d56
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-06-06 23:31:22 -0400

    Working updates to fluence.main_isf

--- +fluence/main_isf.m
+++ +fluence/main_isf.m
@@ -12,7 +12,6 @@ l = [442,716]; % measurement wavelengths
 opts = [];
 % opts.hv = 'constant';
 opts.Em = 'default'; %'Krishnan'; %'Mie-Krishnan';
-opts.ann = 'Michelsen';
 
 % prop = props.x_apb17_Fe;
 prop0 = props.x_ldf;
@@ -27,6 +26,30 @@ prop0.Ti = 298;
 prop0.Tg = prop0.Ti;
 prop0.sigma = 0;  % 0.1
 
+prop = prop0;  % copy to simple model, prior to changes
+
+%-- Change true model parameters -----------------------------------------%
+% Case 1.
+% prop0.Em = @(l,dp) 0.8 .* ones(1,length(l));
+
+% Case 2: Annealing.
+%{
+opts.ann = 'Sipkens';
+bet0 = 28.72;  % soot
+zet0 = 0.83;
+bet1 = 0.626;  % graphite
+zet1 = 1.186;
+fun_Em = @(l,dp,bet,zet) (l .* 1e-6) .^ (1-zet) .* bet ./ (6*pi);
+prop0.Em = @(l,dp,X) fun_Em(l,dp,bet0,zet0);
+prop0.Emr = @(l1,l2,dp) prop0.CEmr.*prop0.Em(l1,dp)./prop0.Em(l2,dp);
+%}
+
+% Case 3.
+% See loop below.
+
+% Case 4.
+%-------------------------------------------------------------------------%
+
 % Define models and their parameterizations.
 opts.abs = 'include';  % set opts to include absorption
 x_fields = {'dp0', 'F0', 'CEmr'};  % set models to take only diameter as inputs
@@ -38,8 +61,6 @@ smodel = SModel(prop0, x_fields, t, l);
 smodel.htmodel = htmodel;
 
 % Simply SModel.
-prop = prop0;
-prop.Em = @(l,dp) 0.8 .* ones(1,length(l));
 smodels = SModel(prop, x_fields, t, l);
 smodels.htmodel = htmodel;
 
@@ -49,7 +70,8 @@ disp(' ');
 [~, prompt] = min(abs(t - 20));
 
 nf = 60;
-F0_vec = linspace(0.0005, 1, nf);
+% nf = 20;
+F0_vec = linspace(0.0005, 0.6, nf);
 dp = 40;
 
 T = [];  J1 = [];  J2 = []; Cinf = []; Ct = [];
@@ -60,13 +82,27 @@ for ii=1:length(F0_vec)
     
     Jt = smodel.evaluateF([dp, F0_vec(ii), 1]);
     Jt = Jt .* m(:,ii) ./ m(1,ii); % scale by particle mass loss
+    
+    % For Case 3.
+    %{
+    Jt = Jt + smodel.evaluateF([dp, 0, 1]);
+    %}
+
+    % For Case 4.
+    %-{
+    tg = 5;
+    Jt = sum(permute(Jt, [2,1,3]) .* normpdf(t - t' + tg, 0, tg), 2) ./ ...
+        sum(normpdf(t - t', 0, tg), 2);
+    %}
+    
     J1(:,ii) = Jt(:,1,1);  % choose first wavlength
     J2(:,ii) = Jt(:,1,1);  % choose second wavlength
     
     [~, ~, Ct(:,ii)] = smodel.IModel(prop0, Jt);  % inferred ISF
     
+    % Js = smodels.evaluateF([dp, F0_vec(ii), 1]);
     [~, ~, Cinf(:,ii)] = smodels.IModel(prop, Jt);  % inferred ISF
-
+    
     tools.textbar([ii, nf]);
 end
 
